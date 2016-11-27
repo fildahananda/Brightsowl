602734d50aa6c11ec228d4288ba106235c11b9e7
harumlokawati <harumlokawati@yahoo.com>
2016-11-27 18:15:01 +0700

    Add files via upload

---
+++ mesinkata.c
@@ -0,0 +1,94 @@
+/*NIM       : 13515109 */
+/*Nama      : Harum Lokawati*/
+/*Tanggal   : 20 September 2016 */
+/*Topik     : ADT Mesin Kata */
+/*Deskripsi : ADT Mesin kata menggunakan mesin karakter untuk
+              membaca serangkaian karakter pada file eksternal */
+
+#include "mesinkata.h"
+boolean EndKata;
+Kata CKata;
+void IgnoreBlank()
+/* Mengabaikan satu atau beberapa BLANK
+   I.S. : CC sembarang
+   F.S. : CC ≠ BLANK atau CC = MARK */
+{
+    while ((CC == BLANK) && (CC != MARK)&&(CC == '\n'))
+    {
+        ADV();
+    }
+}
+
+void STARTKATA(char filename[])
+/* I.S. : CC sembarang
+   F.S. : EndKata = true, dan CC = MARK;
+          atau EndKata = false, CKata adalah kata yang sudah diakuisisi,
+          CC karakter pertama sesudah karakter terakhir kata */
+{
+    START(filename);
+    IgnoreBlank();
+    if (CC == MARK)
+    {
+        EndKata = true;
+    }
+    else
+    {
+        EndKata = false;
+        SalinKata();
+    }
+}
+
+void ADVKATA()
+/* I.S. : CC adalah karakter pertama kata yang akan diakuisisi
+   F.S. : CKata adalah kata terakhir yang sudah diakuisisi,
+          CC adalah karakter pertama dari kata berikutnya, mungkin MARK
+          Jika CC = MARK, EndKata = true.
+   Proses : Akuisisi kata menggunakan procedure SalinKata */
+{
+    IgnoreBlank();
+    if (CC == MARK)
+    {
+        EndKata = true;
+    }
+    else
+    {
+        SalinKata();
+        IgnoreBlank();
+    }
+}
+
+void SalinKata()
+/* Mengakuisisi kata, menyimpan dalam CKata
+   I.S. : CC adalah karakter pertama dari kata
+   F.S. : CKata berisi kata yang sudah diakuisisi;
+          CC = BLANK atau CC = MARK;
+          CC adalah karakter sesudah karakter terakhir yang diakuisisi.
+          Jika panjang kata melebihi NMax, maka sisa kata "dipotong" */
+{
+    int i = 1;
+    for (;;)
+    {
+		if((CC!=MARK)&&(CC!=BLANK)&&(CC!='\n'))
+		{
+			CKata.TabKata[i]= CC;
+		}
+        ADV();
+        if ((CC == MARK) || (CC == BLANK)||(CC=='\n'))
+        {
+			ADV();
+            break;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    if (i>NMax)
+    {
+        CKata.Length=NMax;
+    }
+    else
+    {
+        CKata.Length = i;
+    }
+}
